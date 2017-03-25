632b3465b7052c6a1d3c6bb7a05412826db2278d
Erick Wendel Gomes da Silva <erick.silva@fcamara.com.br>
2017-03-25 02:43:46 -0300

    async errors manipulation

--- es8/async-await.js
+++ es8/async-await.js
@@ -1,21 +1,27 @@
 'use strict'
-function getUserName () {
-    return new Promise((resolve, reject) => 
-            setTimeout(() => resolve('Erick')))
+function getUserName() {
+    return new Promise((resolve, reject) =>
+        setTimeout(() => resolve('Erick')))
 }
-function saveUser (userName) {
-    return new Promise((resolve, reject) => 
-            setTimeout(() => resolve({userName, date: '04/25/1995'})))
+function saveUser(userName) {
+ 
+    return new Promise((resolve, reject) =>
+        setTimeout(() => reject({ userName, date: '04/25/1995' })))
 }
 
 async function run() {
-    const userName = await getUserName() 
-    return await saveUser(userName)
-} 
+    try {
+        const userName = await getUserName()
+        return await saveUser(userName)
+    }
+    catch (e) {
+        throw e
+    }
+}
 
 run()
     .then(success => console.log(`Success: ${JSON.stringify(success)}`))
-    .catch(error => console.error(`Error: ${error}`))
+    .catch(error => console.error(`Error: ${JSON.stringify(error)}`))
 //Success: {"userName":"Erick","date":"04/25/1995"}
 
 
